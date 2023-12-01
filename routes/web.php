31b4302103b54d6d6837cf72d045793290b94995
IcegnawerTest <150777415+IcegnawerTest@users.noreply.github.com>
2023-12-01 14:08:00 +0500

    Обновление

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,30 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\ApplicationController;
-use App\Http\Controllers\CourseController;
-use App\Http\Controllers\UserController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
-Route::get('/', [CourseController::class, "index"]);
+use App\Http\Controllers\CartController;
+/*
 
-Route::get('/login', [UserController::class, "signin"]);
-Route::get('/signin', [CourseController::class, "signin_valid"]);
-Route::get('/signout', [UserController::class, "signout"]);
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get('/account', [UserController::class, "account"]);
+Route::get('/', [ProductController::class,'index'])->name('index');
+Auth::routes();
+Route::get('/products', [ProductController::class,'products'])->name('products');
 
-Route::get('/registration', [UserController::class, "signup"]);
-Route::post('/signup', [UserController::class, "signup_valid"]);
+Route::get('/home', [ProductController::class, 'index'])->name('index');
+Route::get('/{product}', [ProductController::class, 'detail'])->name('detail');
 
-Route::post("/enroll",[ApplicationController::class, "create_application"]);
-
-Route::get("/admin",[AdminController::class,"index"]);
-Route::post("/cours",[CourseController::class,"create_course"]);
-Route::post("/category",[CourseController::class,"create_categories"]);
-
-
-Route::get("/application/{id_application}/confirm",[ApplicationController::class,"confirm"]);
+    Route::get('/cart/index', [CartController::class, 'index'])->name('cart.index');
+    Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
+    Route::post('/cart/remove', [CartController::class, 'remove'])->name('cart.remove');
---
+++ storage/framework/views/f48d00e1e86354385fabae3dc54f9192cddb3471.php
@@ -0,0 +1,14 @@
+<?php $__env->startSection('title', 'Пекарня'); ?>
+
+<?php $__env->startSection('content'); ?>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+<?php echo $__env->make('pattern.App', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\OSPanel\domains\BakeryUld\resources\views/index.blade.php ENDPATH**/ ?>
